c47bb8af28ecb3e0c834c62c2306f0f3c4eef1b9
ИСП-41 <-41@LAB27-03>
2024-09-11 13:09:52 +0300

    Добавьте файлы проекта.

---
+++ Prakt1/Program.cs
@@ -0,0 +1,47 @@
+﻿// Выяснить сколько положительных элементов содержит матрица
+// размерности n * m , если a ij = sin(i+j/2)
+
+double[,] matrix;
+
+Console.Write("Введите количество столбцов матрицы: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество строк матрицы: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+matrix = new double[m, n];
+matrix = FillAndPrintMatrix(matrix);
+int count = GetPositiveElements(matrix);
+Console.Write($"Количество положительных элементов: {count}");
+Console.ReadKey();
+
+int GetPositiveElements(double[,] matrix)
+{
+    int count = 0;
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] > 0)
+                count++;
+        }
+    }
+
+    return count;
+}
+
+double[,] FillAndPrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Sin(i + j / 2);
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+
+    return matrix;
+}
